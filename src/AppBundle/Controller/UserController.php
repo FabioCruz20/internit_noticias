78a916ddd6c776089404ece6d65b9016fdc5ef58
Fabio Cruz <fabiovict95@gmail.com>
2019-08-25 22:16:22 -0300

    Implementa lista e atualização de usuários

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -41,4 +41,44 @@ class UserController extends Controller
             "form" => $form->createView()
         ]);
     }
+
+    /**
+     * @Route("/admin/user", name="user.admin")
+     */
+    public function userListAction() {
+
+        $repository = $this->getDoctrine()->getRepository(User::class);
+
+        $usuarios = $repository->findAll();
+
+        return $this->render("admin/user.html.twig", [
+            "usuarios" => $usuarios
+        ]);
+    }
+
+    /**
+     * @Route("/admin/user/edit/{id}", name="user.edit")
+     */
+    public function userEditAction(User $usuario, Request $request) {
+
+        $form = $this->createForm(UserType::class, $usuario);
+        $form->handleRequest($request);
+
+        if ($form->isValid() && $form->isSubmitted()) {
+
+            // entity manager para persistir o usuário
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($usuario);
+            $em->flush();
+
+            $this->addFlash("success", "Usuário atualizado.");
+
+            return $this->redirect($this->generateUrl("user.admin"));
+        }
+
+        return $this->render("admin/user-edit.html.twig", [
+            "form" => $form->createView(),
+            "funcao" => "Atualizar"
+        ]);
+    }
 }
